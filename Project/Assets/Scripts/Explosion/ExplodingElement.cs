6246ff32e1a408b057fc9699c1d9aba58cf42042
Meetic666 <bellay.quentin@gmail.com>
2015-06-27 08:59:34 -0400

    Added functionality to explosion and exploding stuff

---
+++ Project/Assets/Scripts/Explosion/ExplodingElement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplodingElement : MonoBehaviour
+{
+	public GameObject m_CollisionEffectsPrefab;
+
+	Rigidbody m_Rigidbody;
+
+	// Use this for initialization
+	void Start () 
+	{
+		m_Rigidbody = GetComponent<Rigidbody>();
+	}
+
+	public void Explode(Vector3 positionOfExplosion, float radiusOfExplosion, float forceOfExplosion)
+	{
+		if(m_Rigidbody != null)
+		{
+			m_Rigidbody.isKinematic = false;
+
+			m_Rigidbody.AddExplosionForce(forceOfExplosion, positionOfExplosion, radiusOfExplosion);
+		}
+	}
+
+	void OnCollisionEnter(Collision collisionInfo)
+	{
+		if(m_CollisionEffectsPrefab != null)
+		{
+			Instantiate (m_CollisionEffectsPrefab, collisionInfo.contacts[0].point, Quaternion.identity);
+		}
+	}
+}
---
+++ Project/Assets/Scripts/Explosion/Explosion.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explosion : MonoBehaviour 
+{
+	public float m_ExplosionRadius;
+	public float m_ExplosionForce;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Collider[] colliders = Physics.OverlapSphere(transform.position, m_ExplosionRadius);
+
+		foreach(Collider otherCollider in colliders)
+		{
+			ExplodingElement explodingElement = otherCollider.GetComponent<ExplodingElement>() ;
+
+			if(explodingElement != null)
+			{
+				explodingElement.Explode(transform.position, m_ExplosionRadius, m_ExplosionForce);
+			}
+		}
+	}
+}
