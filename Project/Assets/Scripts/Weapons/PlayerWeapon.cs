07abbb32454cafe94638c705f84a2b0615b7c115
Meetic666 <bellay.quentin@gmail.com>
2015-06-28 01:50:28 -0400

    Added jet sound

--- Project/Assets/Scripts/Weapons/PlayerWeapon.cs
+++ Project/Assets/Scripts/Weapons/PlayerWeapon.cs
@@ -9,7 +9,7 @@ public class PlayerWeapon : Weapon
 	// Update is called once per frame
 	protected override void update ()
     {
-        if (Input.GetKey(FireKey) || Input.GetButton(FireButton))
+        if (Time.timeScale != 0.0f && (Input.GetKey(FireKey) || Input.GetButton(FireButton)))
         {
             Fire();
         }
